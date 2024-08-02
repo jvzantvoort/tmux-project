d8d6ab6277fe7a9dfbb5575bb97a83debc45d764
John van Zantvoort <john@vanzantvoort.org>
2024-08-02 23:58:25 +0200

    minor: reshuffle print

--- project/list.go
+++ project/list.go
@@ -3,92 +3,55 @@ package project
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/jvzantvoort/tmux-project/config"
 	"github.com/jvzantvoort/tmux-project/sessions"
 	"github.com/jvzantvoort/tmux-project/tmux"
 	"github.com/jvzantvoort/tmux-project/utils"
 	"github.com/olekukonko/tablewriter"
-	log "github.com/sirupsen/logrus"
 )
 
-type ListTable struct {
-	Name        string
-	Description string
-	Workdir     string
-	Sane        bool
+func ListActive() []string {
+	tmux := tmux.NewTmux()
+	retv, _ := tmux.ListActive()
+	return retv
 }
 
 func PrintFullList() {
-	tmux := tmux.NewTmux()
-	active, _ := tmux.ListActive()
+	active := ListActive()
+	sessiondata, err := sessions.ListTmuxConfigs(config.SessionDir())
+	utils.ErrorExit(err)
+
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"Name", "Active", "Description", "Workdir", "Sane"})
 
-	for _, target := range ListTmuxConfigs() {
-		if target.Name == "default" {
+	for _, entries := range sessiondata {
+		if entries[0] == "default" {
 			continue
 		}
-		var cols []string
-		cols = append(cols, target.Name)
 
-		if utils.StringInSlice(target.Name, active) {
-			cols = append(cols, "yes")
-		} else {
-			cols = append(cols, "")
-		}
-		cols = append(cols, target.Description)
-		cols = append(cols, target.Workdir)
-		if target.Sane {
-			cols = append(cols, "true")
-		} else {
-			cols = append(cols, "false")
+		// create cols
+		cols := []string{entries[0], ""}
+		cols = append(cols, entries[1:]...)
 
+		if utils.StringInSlice(entries[0], active) {
+			cols[1] = "yes"
 		}
+
 		table.Append(cols)
 	}
+
 	table.SetHeaderLine(true)
 	table.SetBorder(false)
 	table.Render()
 }
 
 func PrintShortList() {
-	for _, target := range ListTmuxConfigs() {
-		fmt.Println(target.Name)
+	sessiondata, err := sessions.ListTmuxConfigs(config.SessionDir())
+	utils.ErrorExit(err)
+
+	for _, item := range sessiondata {
+		fmt.Printf("%s\n", item[0])
+
 	}
-}
-
-func ListTmuxConfigs() []ListTable {
-	var retv []ListTable
-	targets, err := os.ReadDir(config.SessionDir())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, target := range targets {
-		target_name := target.Name()
-
-		// we only want the session names
-		if !strings.HasSuffix(target_name, ".rc") {
-			continue
-		}
-
-		// "common" is shared by all others
-		if target_name == "common.rc" {
-			continue
-		}
-
-		target_name = strings.TrimSuffix(target_name, ".rc")
-
-		session := sessions.NewTmuxSession(target_name)
-
-		t := ListTable{}
-		t.Name = session.Name
-		t.Description = session.Description
-		t.Workdir = session.Workdir
-		t.Sane = session.IsSane()
-		retv = append(retv, t)
-	}
-	return retv
 }
--- sessions/tmuxsessions.go
+++ sessions/tmuxsessions.go
@@ -2,6 +2,7 @@ package sessions
 
 import (
 	"os"
+	"sort"
 	"strings"
 
 	"github.com/jvzantvoort/tmux-project/config"
@@ -12,13 +13,44 @@ type TmuxSessions struct {
 	Sessions []TmuxSession
 }
 
-func NewTmuxSessions() *TmuxSessions {
-	tmux_sessions := &TmuxSessions{}
+// Define a type for a slice of slices of strings
+type SliceOfStringSlices [][]string
 
-	// list ~/.tmux.d
-	targets, err := os.ReadDir(config.SessionDir())
+// Implement the sort.Interface for the type
+func (s SliceOfStringSlices) Len() int {
+	return len(s)
+}
+
+func (s SliceOfStringSlices) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s SliceOfStringSlices) Less(i, j int) bool {
+	// Compare the first elements of the sub-slices (strings)
+	return s[i][0] < s[j][0]
+}
+
+func NewTmuxSessions() *TmuxSessions {
+	retv := &TmuxSessions{}
+
+	sess, err := ListSessions(config.SessionDir())
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	for _, item := range sess {
+		retv.Sessions = append(retv.Sessions, item)
+	}
+	return retv
+
+}
+
+func ListSessions(inputdir string) (map[string]TmuxSession, error) {
+	retv := make(map[string]TmuxSession)
+	targets, err := os.ReadDir(inputdir)
+	if err != nil {
+		log.Fatal(err)
+		return retv, err
 	}
 
 	for _, target := range targets {
@@ -33,14 +65,29 @@ func NewTmuxSessions() *TmuxSessions {
 		if target_name == "common.rc" {
 			continue
 		}
-
 		target_name = strings.TrimSuffix(target_name, ".rc")
 
-		x := NewTmuxSession(target_name)
-		tmux_sessions.Sessions = append(tmux_sessions.Sessions, *x)
+		retv[target_name] = *NewTmuxSession(target_name)
 
 	}
+	return retv, nil
 
-	return tmux_sessions
+}
 
+func ListTmuxConfigs(inputdir string) ([][]string, error) {
+	retv := [][]string{}
+
+	sessiondata, err := ListSessions(inputdir)
+
+	for _, session := range sessiondata {
+		sane := "false"
+		if session.IsSane() {
+			sane = "true"
+		}
+		cols := []string{session.Name, session.Description, session.Workdir, sane}
+		retv = append(retv, cols)
+	}
+	sort.Sort(SliceOfStringSlices(retv))
+
+	return retv, err
 }
