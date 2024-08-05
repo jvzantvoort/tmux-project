aa24ed429d2e050b72fd11ed3507695c27a9db75
John van Zantvoort <john@vanzantvoort.org>
2024-08-05 19:42:39 +0200

    new: project add Archive command

---
+++ project/project_archive.go
@@ -0,0 +1,46 @@
+package project
+
+import (
+	"github.com/jvzantvoort/tmux-project/archive"
+	"github.com/jvzantvoort/tmux-project/utils"
+)
+
+func (proj Project) ListFiles() []string {
+	utils.LogStart()
+	defer utils.LogEnd()
+
+	retv := []string{}
+
+	utils.Debugf("  add %#v", proj.ProjectDir)
+	retv = append(retv, proj.ProjectDir)
+
+	utils.Debugf("  add %#v", proj.ProjectConfigFile())
+	retv = append(retv, proj.ProjectConfigFile())
+
+	for _, proj_target := range proj.Targets {
+		dest := proj.CalcDestination(proj_target.Destination)
+		utils.Debugf("  add %s (%s)", dest, proj.ProjectDir)
+		retv = append(retv, dest)
+	}
+	return retv
+}
+
+func (proj Project) Archive(archivedescr string) error {
+	utils.LogStart()
+	defer utils.LogEnd()
+
+	if archivedescr == ".tar.gz" {
+		utils.Abort("no name for archive")
+	}
+
+	archivename, _ := utils.Expand(proj.Parse(archivedescr))
+
+	utils.Debugf("archive name: %s", archivename)
+
+	tar := archive.NewTarArchive(archivename)
+
+	tar.AddFiles(proj.ListFiles())
+
+	return tar.CreateArchive()
+
+}
