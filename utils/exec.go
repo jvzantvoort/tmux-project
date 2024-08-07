e4355934bb5287963bfa9b1b0311ec1bf1dec005
John van Zantvoort <john@vanzantvoort.org>
2024-08-07 23:28:10 +0200

    new: added ExecSilent for command that do not need error checking

--- utils/exec.go
+++ utils/exec.go
@@ -109,3 +109,49 @@ func Exec(cwd, args string) ([]string, []string, error) {
 
 	return stdout_list, stderr_list, eerror
 }
+
+// ExecSilent execute a command but don't care too much about the result.
+func ExecSilent(cwd, args string) ([]string, []string, error) {
+	commandlist := []string{}
+	stdout_list := []string{}
+	stderr_list := []string{}
+
+	cmndargs := strings.Split(args, " ")
+
+	cmnd := cmndargs[0]
+	cmndargs = cmndargs[1:]
+	commandlist = append(commandlist, cmndargs...)
+	command := exec.Command(cmnd, commandlist...)
+	command.Dir = cwd
+
+	stdout, _ := command.StdoutPipe()
+	stderr, _ := command.StderrPipe()
+
+	err := command.Start()
+	if err != nil {
+		return stdout_list, stderr_list, err
+	}
+
+	stdout_scan := bufio.NewScanner(stdout)
+	stdout_scan.Split(bufio.ScanLines)
+	for stdout_scan.Scan() {
+		msg := stdout_scan.Text()
+		Debugf("stdout: %s", msg)
+		stdout_list = append(stdout_list, msg)
+	}
+
+	stderr_scan := bufio.NewScanner(stderr)
+	stderr_scan.Split(bufio.ScanLines)
+	for stderr_scan.Scan() {
+		msg := stderr_scan.Text()
+		Debugf("stderr: %s", msg)
+		stderr_list = append(stderr_list, msg)
+	}
+
+	eerror := command.Wait()
+	if eerror != nil {
+		Debugf("Error: command failed, %v", err)
+	}
+
+	return stdout_list, stderr_list, eerror
+}
