4df9565e9bfb68bce625c213ead1098341c92f3d
John van Zantvoort <john@vanzantvoort.org>
2024-08-07 23:30:27 +0200

    fix: remove unneeded viper

--- cmd/tmux-project/constants.go
+++ cmd/tmux-project/constants.go
@@ -7,13 +7,4 @@ const (
 	ConstDirMode int = 0755
 	// ConstFileMode permissions on newly created configuration files
 	ConstFileMode int = 0644
-
-	// EnvConfigDir environment pointing to the main config dir
-	EnvConfigDir string = "TMUX_PROJECT_CONFIG_DIR"
-
-	EnvLogDir  string = "TMUX_PROJECT_OUTPUT_DIR"
-	EnvLogName string = "TMUX_PROJECT_OUTPUT_NAME"
-
-	DefaultLogDir string = "~/Logs"
-	DefaultLogName string = "tmux.log"
 )
