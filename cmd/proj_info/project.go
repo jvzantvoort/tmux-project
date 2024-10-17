00d14dd7d8ce09d3ae9f800ed77bcd7a08e47cca
John van Zantvoort <john@vanzantvoort.org>
2024-10-17 20:08:29 +0200

    feat: update proj_info (#21)

--- cmd/proj_info/project.go
+++ cmd/proj_info/project.go
@@ -1,94 +1,60 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
+	"sync"
 
-	"github.com/fatih/color"
-	log "github.com/sirupsen/logrus"
-
-	"github.com/jvzantvoort/tmux-project/git"
 	"github.com/jvzantvoort/tmux-project/utils"
 )
 
-type ProjectDef struct {
-	Name     string
-	AbsPath  string
-	Path     string
-	Branch   string
-	Expected bool
-	Status   map[string]int
-	SubPath  string
-	Chapter  string
-	Info     os.FileInfo
-}
-
-func (p ProjectDef) GetFields() []string {
-	retv := []string{}
-	retv = append(retv, p.Name)
-
-	br_col := color.New(BranchChangedColor)
-	if utils.StringInSlice(p.Branch, []string{"master", "main", "develop"}) {
-		br_col = color.New(BranchDefaultColor)
-	}
-	var stat_str string
-	for status, amount := range p.Status {
-		stat_str = fmt.Sprintf("%s %s:%d", stat_str, status, amount)
-		stat_str = strings.TrimSpace(stat_str)
-	}
-	if len(stat_str) != 0 {
-		retv = append(retv, fmt.Sprintf("[%s]", stat_str))
-	} else {
-		retv = append(retv, " ")
-	}
-
-	retv = append(retv, br_col.Sprint(p.Branch))
-
-	return retv
-}
-
-func NewProjectDef(projdir, dirname string) *ProjectDef {
-	retv := &ProjectDef{}
-
-	retv.Path = dirname
-	retv.Info, _ = os.Lstat(dirname)
-	if projdir == dirname {
-		retv.Name = "."
-		retv.Chapter = "root"
-	} else {
-		retv.Name = dirname[len(projdir)+1:]
-		retv.Chapter = "rest"
-	}
-
-	gitcmnd := git.NewGitCmd(retv.Path)
-	if gitcmnd.IsGit() {
-		log.Debugf("%s is a gitdir", retv.Path)
-		retv.Branch, _ = gitcmnd.Branch()
-		retv.Status = gitcmnd.GetStatus()
-	}
-	return retv
-}
-
-func findAllProjects(projdir string) []ProjectDef {
-	var retv []ProjectDef
+func walkAllProjects(projdir string) ([]string, error) {
+	var retv []string
+	utils.LogStart()
+	defer utils.LogEnd()
 
 	err := filepath.Walk(projdir, func(file string, fi os.FileInfo, inerr error) error {
 		err := inerr
 		if err != nil {
-			log.Errorf("this passed an error: %q", err)
+			utils.Errorf("this passed an error: %q", err)
 		}
 		if fi.IsDir() && fi.Name() == ".git" {
-			dirname := filepath.Dir(file)
-			repos := NewProjectDef(projdir, dirname)
-			retv = append(retv, *repos)
+			retv = append(retv, filepath.Dir(file))
 		}
 		return nil
 	})
-	if err != nil {
-		log.Errorf("Error: %s", err)
+
+	return retv, err
+}
+
+func findAllProjects(projdir string) []ProjectDef {
+	var data []ProjectDef
+	var retv []ProjectDef
+	utils.LogStart()
+	defer utils.LogEnd()
+
+	var wg sync.WaitGroup
+
+	dirnames, _ := walkAllProjects(projdir)
+
+	for _, dirname := range dirnames {
+		repos := NewProjectDef(projdir, dirname)
+		data = append(data, *repos)
 	}
+
+	for _, obj := range data {
+		wg.Add(1)
+
+		go func(obj ProjectDef) {
+			defer wg.Done()
+			obj.Init()
+			retv = append(retv, obj)
+		}(obj)
+
+	}
+	// Wait for all goroutines to finish
+	wg.Wait()
+
 	return retv
 
 }
