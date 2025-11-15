a4316fde1368b45fbe5687a724874e2507d54f0b
John van Zantvoort <john@vanzantvoort.org>
2025-11-15 15:17:20 +0100

    feat: add status to list and steamline listing

--- project/list.go
+++ project/list.go
@@ -3,6 +3,7 @@ package project
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 
 	"github.com/jvzantvoort/tmux-project/config"
@@ -39,43 +40,116 @@ func ListConfigs() []string {
 	return retv
 }
 
-// PrintFullList prints the list of sessions
-func PrintFullList() {
+func GenerateListOfMaps() []map[string]string {
 	var err error
 	active, _ := tmux.ListActive()
-
-	table := tablewriter.NewWriter(os.Stdout)
-	table.Header([]string{"Name", "Active", "Description", "Directory", "Sane"})
+	data := []map[string]string{}
 
 	for _, sessionname := range ListConfigs() {
 		sessp := NewProject(sessionname)
 		err = sessp.Open()
 		if err != nil {
-			fmt.Printf("%#v\n", err)
+			utils.Errorf("%#v", err)
 			continue
 		}
-		cols := []string{}
-		cols = append(cols, sessionname)
+
+		dict := map[string]string{}
+		dict["Name"] = sessionname
+		dict["Type"] = sessp.ProjectType
+		dict["Description"] = sessp.Description
+		dict["Directory"] = sessp.Directory
+		dict["Status"] = sessp.Status
+
 		if utils.StringInSlice(sessionname, active) {
-			cols = append(cols, "yes")
+			dict["Active"] = "yes"
 		} else {
-			cols = append(cols, "")
+			dict["Active"] = ""
 		}
-		cols = append(cols, sessp.Description)
-		cols = append(cols, sessp.Directory)
+
 		sane := "true"
 		for _, target := range sessp.ListFiles() {
 			if !utils.TargetExists(target) {
 				sane = "false"
 			}
 		}
-		cols = append(cols, sane)
-		table.Append(cols)
+		dict["Sane"] = sane
 
+		data = append(data, dict)
 	}
+	return data
 
-	table.Render()
+}
 
+func indexOf(target string, list []string) int {
+	for i, v := range list {
+		if v == target {
+			return i
+		}
+	}
+	return -1 // not found
+}
+
+func resortSlice(PrimaryColumn, SecondaryColumn int, rows [][]string) [][]string {
+	sort.Slice(rows, func(i, j int) bool {
+		name1 := rows[i][PrimaryColumn]
+		name2 := rows[j][PrimaryColumn]
+		type1 := rows[i][SecondaryColumn]
+		type2 := rows[j][SecondaryColumn]
+
+		if type1 != type2 {
+			return type1 < type2
+		}
+		return name1 < name2
+	})
+	return rows
+}
+
+func GenerateFullList(orderField int, fields ...string) [][]string {
+	retv := [][]string{}
+	for _, row := range GenerateListOfMaps() {
+		tmplist := []string{}
+		for _, field := range fields {
+			if value, ok := row[field]; ok {
+				tmplist = append(tmplist, value)
+			} else {
+				tmplist = append(tmplist, "")
+			}
+		}
+		retv = append(retv, tmplist)
+	}
+	return retv
+}
+
+// PrintFullList prints the list of sessions
+func PrintFullList() {
+	legend := []string{"Name", "Type", "Status", "Active", "Description", "Directory"}
+
+	SecondaryColumn := indexOf("Type", legend)
+
+	rows := GenerateFullList(SecondaryColumn, legend...)
+	rows = resortSlice(0, SecondaryColumn, rows)
+
+	outputtable := tablewriter.NewWriter(os.Stdout)
+	outputtable.Header(legend)
+	outputtable.Bulk(rows)
+	outputtable.Render()
+
+}
+
+// PrintFullList prints the list of sessions
+func PrintSanityList() {
+
+	legend := []string{"Name", "Type", "Status", "Sane", "Description", "Directory"}
+
+	SecondaryColumn := indexOf("Type", legend)
+
+	rows := GenerateFullList(SecondaryColumn, legend...)
+	rows = resortSlice(0, SecondaryColumn, rows)
+
+	outputtable := tablewriter.NewWriter(os.Stdout)
+	outputtable.Header(legend)
+	outputtable.Bulk(rows)
+	outputtable.Render()
 }
 
 // PrintShortList prints the list of sessions
